85bbf742bcf8dc1f6b473f92464d4cdff07bf166
fercuacas <fernando2002cuadrado@gmail.com>
2025-08-28 08:40:39 +0200

    Organize src into domain-specific directories

--- launch/robofer.launch.py
+++ launch/robofer.launch.py
@@ -2,10 +2,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='robofer', executable='eyes_node',   name='eyes'),
-        Node(package='robofer', executable='servos_node', name='servos'),
-        Node(package='robofer', executable='sound_node',  name='sound'),
-        Node(package='robofer', executable='buttons_node',name='buttons'),
+        Node(package="robofer", executable="eyes_unified_node", name="eyes"),
+        Node(package="robofer", executable="state_handler_node", name="state_handler"),
+        Node(package="robofer", executable="buttons_node", name="buttons"),
     ])
+
