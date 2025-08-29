70f3ca19091b92808f43f47be2002054cfb575f3
fercuacas <fernando2002cuadrado@gmail.com>
2025-08-29 19:49:35 +0200

    launch

--- launch/robofer.launch.py
+++ launch/robofer.launch.py
@@ -10,5 +10,7 @@ def generate_launch_description():
         Node(package="robofer", executable="buttons_node", name="buttons"),
         Node(package="robofer", executable="wifi_manager_node", name="wifi_manager"),
         Node(package="robofer", executable="bluetooth_manager_node", name="bluetooth_manager"),
+        Node(package="robofer", executable="bt_provision_node", name="bt_provision")
+
     ])
 
